ed78b38cbc9259214793815891e22c05be88874b
494130947@qq.com <494130947@qq.com>
2022-01-25 22:32:41 +0800

    chore: update prettierrc

--- vite-project-framework/.prettierrc.js
+++ vite-project-framework/.prettierrc.js
@@ -5,5 +5,5 @@ module.exports = {
 	singleQuote: false, //字符串是否使用单引号，默认为false，使用双引号
 	semi: true, //行位是否使用分号，默认为true
 	trailingComma: "none", //是否使用尾逗号，有三个可选值"<none|es5|all>"
-	bracketSpacing: true, //对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
+	bracketSpacing: true //对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
 };
