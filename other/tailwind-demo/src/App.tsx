0e1a5f955c849c75dcb7635a12d815872649bda9
sanyuan0704 <494130947@qq.com>
2022-04-13 20:29:54 +0800

    chore: update tailwind demo

--- other/tailwind-demo/src/App.tsx
+++ other/tailwind-demo/src/App.tsx
@@ -1,42 +1,12 @@
-import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <header className="App-header">
         <img src={logo} className="w-20" alt="logo" />
         <p className="bg-red-400">Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
       </header>
     </div>
   );
